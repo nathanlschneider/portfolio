7e3447ebc0e3d6e7a036fdd7437a5d874135b7fe
nathanlschneider <nschneider@mhgi.net>
2018-10-20 03:12:30 -0400

    v2

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,37 @@
-'use strict';
-const app = (function(){
+"use strict";
 
-const bar = document.querySelectorAll('.bar');
+const app = (function() {
+
+    let nav = document.getElementById('nav');
+    let menuButton = document.getElementById('menuButton');
+    let letters = document.querySelectorAll('.secondary');
+    let space = document.querySelector('.space');
+
+    window.addEventListener("scroll", function (event) {
+        var scroll = this.scrollY;
+        if (scroll > 100) { 
+            nav.classList.add('shrink');
+            menuButton.classList.add('show');
+            space.classList.remove('space');
+            letters.forEach(letter => letter.classList.add('fade'))
+        } else {
+            nav.classList.remove('shrink');
+            nav.classList.remove('drop');
+            menuButton.classList.remove('show');
+            space.classList.add('space');
+            letters.forEach(letter => letter.classList.remove('fade'))
+
+        }
+    });
+
+    menuButton.addEventListener('click', e => {
+      
+        if (window.getComputedStyle(menuButton).opacity > 0) {
+        nav.classList.toggle('drop');
+
+        }
+    })
+   
+
 
 })();
